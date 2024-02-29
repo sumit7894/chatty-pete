580a03d04493c72c6f09eea766ba2f99fa5ab6ba
Sumit Maurya <smithdashdash@gmail.com>
2024-03-01 00:49:32 +0530

    added a logout button

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,51 @@
 import Head from "next/head";
 import Link from "next/link";
-
+import {useUser} from '@auth0/nextjs-auth0/client'
+import { redirect } from "next/dist/server/api-utils";
+import { getSession } from "@auth0/nextjs-auth0";
 export default function Home() {
+  const {isLoading,error,user} = useUser();
+
+  if(isLoading) return <div>Loading ...</div>
+  if(error) return <div>{error.message}</div>
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Chatty Pete --Login or Signup</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-      <div>
-        <Link href="/api/auth/login">Login</Link>
+      <div className="flex justify-center items-center min-h-screen w-full bg-gray-800 text-white text-center">
+        <div>
+        {!!user && <Link href="/api/auth/logout">Logout</Link>}
+        {!user && (
+        <>
+        <Link href="/api/auth/login"
+         className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600">
+          Login
+        </Link>
+        <Link href="/api/auth/signup"
+        className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+        >
+          Sign Up
+        </Link>
+        </>
+        )}
+        </div>
       </div>
-    </div>
+    </>
   );
+}
+
+export const getServerSideProps = async (ctx)=>{
+  const session = await getSession(ctx.req,ctx.res);
+  if(!!session)
+  {
+    return{
+      redirect:{
+        destination:"/chat"
+      }
+    }
+  }
+  return {
+    props:{},
+  }
 }
